135b70f47d4c30913278bd95abebd2ad9597e973
Tom <bicycle.tom@gmail.com>
2015-03-05 18:48:05 -0500

    Update make_projection_cam.py

--- make_projection_cam.py
+++ make_projection_cam.py
@@ -2,24 +2,28 @@ import sys
 import fnmatch
 import hou
 
-def main():
-    nodes = hou.selectedNodes()
-    projector = hou.node("/obj").createNode("cam", "ProjectionCam")
+
+def main(selected_nodes):    
+    nodes = [node for node in selected_nodes if node.type().name() == "mrx::mrx_Chan_Camera::1.0"]
     if not nodes:
-        print "no nodes selected, please select a camera node to continue"
-        return None
-    for node in nodes: 
-        if node.type().name() == "cam":
-            print "this IS a camera"
-        else:
-            print "this is NOT camera"
+        hou.ui.displayMessage("please select a shot camera node to continue")
+        return None    
+    for node in nodes:
+        projector = hou.node("/obj").createNode("cam", "ProjectionCam")
+        children = node.children()
+        cam_children = [node for node in children if node.type().name() == "mrx::mrx_Basic_Camera::1.0"] 
+        for cam_child in cam_children:
+            focal = cam_child.parm("focal").evalAsString()
+            resx = cam_child.parm("resx").evalAsString()
+            resy = cam_child.parm("resy").evalAsString()
+            if focal > 0:
+                projector.parm("focal").set(focal)
+                projector.parm("resx").set(resx)
+                projector.parm("resy").set(resy)      
+            else:
+                print "fail"
+                return None
+    return None
 
-    projector = hou.node("/obj").createNode("cam", "ProjectionCam")
-    projector.parm('focal').set(85)
-    projector.parm('resx').set(2016)
-    projector.parm('resy').set(1134)     
-
-
-
-
-main()
+selected_nodes = hou.selectedNodes()  
+main(selected_nodes)
