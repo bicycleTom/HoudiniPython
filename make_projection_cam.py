d93602a6098f009c2656c7f592032ff9493d4b44
Tom <bicycle.tom@gmail.com>
2015-03-09 00:20:49 -0400

    Update make_projection_cam.py

--- make_projection_cam.py
+++ make_projection_cam.py
@@ -33,6 +33,15 @@ def main(selected_nodes):
                 print "fail"
                 return None
     return None
+    
+def vop(selected_nodes):
+
+    proj_vop = hou.node("/shop").createNode("vopsurface", "test_frame_" + str(int(hou.frame())) + "_" + str(0), run_init_scripts=False)
+    proj_null = proj_vop.createNode("ctransform")
+    proj_text = proj_vop.createNode("texture")
+    print proj_null.parmTuple("from").eval()
+    return None 
 
 selected_nodes = hou.selectedNodes()  
 main(selected_nodes)
+vop(selected_nodes)
